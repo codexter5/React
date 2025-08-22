208b31763ec7f44a4e84a0557c67ca3479936e54
codexter5 <chumalungchamling@gmail.com>
2025-08-22 12:12:23 +0545

    PUSH

--- my_shop/src/pages/Contacts.jsx
+++ my_shop/src/pages/Contacts.jsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material'
+import { Button, Grid, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 const Contacts = () => {
@@ -14,6 +14,10 @@ const Contacts = () => {
         </Grid>
         <Grid size={{xs: 12, md: 6}} bgcolor={'warning.light'}>
             <Typography variant="h4">Contact Form</Typography>
+            <TextField label='E-mail' fullwidth required paceholder='Your email here' helpertext='example: something@somethng.com'/>
+            <TextField label='Subject' fullwidth/>
+            <TextField label='Message' fullwidth multiline rows={4} sx={{marginY: '10px'}}/>
+            <Button variant='contained' fullWidth>Submit</Button>
         </Grid>
         <Grid size={12} bgcolor={'info.light'}>
             Map
