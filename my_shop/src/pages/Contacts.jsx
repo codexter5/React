542892b2516793b58dc32d59b2e232cd0e3c4dd9
codexter5 <chumalungchamling@gmail.com>
2025-08-22 12:01:40 +0545

    Push

--- my_shop/src/pages/Contacts.jsx
+++ my_shop/src/pages/Contacts.jsx
@@ -1,17 +1,24 @@
-import { Grid } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import React from 'react'
 
 const Contacts = () => {
   return (
     <>
-    <Grid container>
-        <Grid>
-
+      <Grid container width={'80%'} margin={'auto'} marginY={5}>
+        <Grid size={{xs: 12, md: 6}} bgcolor={'success.light'}>
+            <Typography variant="h4">Address</Typography>
+            <Typography variant="h4">Kathmandu, Nepal</Typography>
+            <Typography variant="h5">Phone: 01-2312313</Typography>
+            <Typography variant="h6">Email: info@kathford.com</Typography>
+            <Typography variant="h6">Web: www.Kathford.com</Typography>
         </Grid>
-        <Grid>
-            
+        <Grid size={{xs: 12, md: 6}} bgcolor={'warning.light'}>
+            <Typography variant="h4">Contact Form</Typography>
         </Grid>
-    </Grid>
+        <Grid size={12} bgcolor={'info.light'}>
+            Map
+        </Grid>
+      </Grid>
     </>
   )
 }
