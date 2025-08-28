eee54513385e81cb939ed5100259bc72c9683641
codexter5 <chumalungchamling@gmail.com>
2025-08-28 12:24:05 +0545

    Push

--- my_shop/src/components/layouts/MyButton.jsx
+++ my_shop/src/components/layouts/MyButton.jsx
@@ -1,13 +1,11 @@
-import { Button } from '@mui/material'
 import React from 'react'
 
-const MyButton = ({color='blue', text='something'}) => {
+const MyButton = ({color = 'blue',text ='sth',test}) => {
   return (
-    <>
-    <button className='bg${color}-500 px-4 py-2 rounded-lg my-2} onClick={text}'>
+    <button className={`bg-${color}-500 px-4 py-2 rounded-lg my-2 shadow-lg`}
+    onClick={test}>
         {text}
     </button>
-    </>
   )
 }
 
