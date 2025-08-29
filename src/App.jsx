0cd8b98739991861a5dbc5f6f6fea684df64f439
codexter5 <chumalungchamling@gmail.com>
2025-08-29 10:56:16 +0545

    Push

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import MyRouter from './MyRouter'
+import { MyContextProvider } from './components/MyContextProvider'
 
 function App() {
   return (
     <>
+    <MyContextProvider>
+
         <MyRouter></MyRouter>
+
+    </MyContextProvider>
     </>
   )
 }
